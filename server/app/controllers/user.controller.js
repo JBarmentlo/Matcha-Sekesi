d1bac5a168cc7afc2e5526885fe5e96fac7781d4
Joep Barmentlo <JoepB@example.com>
2022-03-17 19:32:29 +0100

    basic login

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -1,61 +1,59 @@
 const { calculateObjectSize } = require("bson");
 const db = require("../newmodels");
+const utils = require("../utils/UserSanitation");
 // console.log(db)
 // console.log("SDF")
-const user_collection 	= db.collection("users");
-const like_collection 	= db.collection("likes");
-const block_collection 	= db.collection("blocks");
-const consult_collection= db.collection("consult");
-const tag_collection 	= db.collection("tags");
+const user_collection = db.collection("users");
+const like_collection = db.collection("likes");
+const block_collection = db.collection("blocks");
+const consult_collection = db.collection("consult");
+const tag_collection = db.collection("tags");
 
 
 // Create and Save a new User
 exports.create = (req, res) => {
 	// Validate request
 	// console.log("req: %s res:%s",req.body, res)
-	if (!req.body.name || !req.body.mail || !req.body.username  || !req.body.firstname || !req.body.password || !req.body.profile_pic) {
-	  res.status(400).send({ message: "Content can not be empty!" });
-	  return;
+	if (!req.body.name || !req.body.mail || !req.body.username || !req.body.firstname || !req.body.password || !req.body.profile_pic) {
+		res.status(400).send({ message: "Content can not be empty!" });
+		return;
 	}
 
 	// Create a User
 	const user = {
-		  username		: req.body.username,
-		  mail			: req.body.mail,
-		  name			: req.body.name,
-		  firstname		: req.body.firstname,
-		  password		: req.body.password,
-		  profile_pic	: req.body.profile_pic,
+		username	:	req.body.username,
+		mail		:	req.body.mail,
+		name		:	req.body.name,
+		firstname	:	req.body.firstname,
+		password	:	req.body.password,
+		profile_pic	:	req.body.profile_pic,
 	};
 
 	// Save User in the database
 	user_collection.insertOne(user)
-	  .then(data => {
-		res.send(data);
-	  })
-	  .catch(err => {
-		res.status(500).send({
-		  message:
-			err.message || "Some error occurred while creating the User."
+		.then(data => {
+			res.send(data);
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while creating the User."
+			});
 		});
-	  });
 };
 
 
 
 checkUsernameSanity = (name) => {
-	try
-	{
-		if (name.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$").length > 0)
-		{
+	try {
+		if (name.match("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$").length > 0) {
 			return true
 		}
-		else
-		{
+		else {
 			console.log("Password %s invalidated (Caps, num, special)")
 			return false
 		}
-		
+
 	}
 	catch
 	{
@@ -72,23 +70,21 @@ exports.check_username_free = (req, res) => {
 	}
 
 	// Save User in the database
-	user_collection.findOne({username: req.body.username})
-	.then(data => {
-		if (data)
-		{
-			res.status(200).send(false)
-		}
-		else
-		{
-			res.status(200).send(true)
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your username"
+	user_collection.findOne({ username: req.body.username })
+		.then(data => {
+			if (data) {
+				res.status(200).send(false)
+			}
+			else {
+				res.status(200).send(true)
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your username"
+			});
 		});
-	});
 };
 
 exports.check_mail_free = (req, res) => {
@@ -100,23 +96,21 @@ exports.check_mail_free = (req, res) => {
 	}
 
 	// Save User in the database
-	user_collection.findOne({mail: req.body.mail})
-	.then(data => {
-		if (data)
-		{
-			res.status(200).send(false)
-		}
-		else
-		{
-			res.status(200).send(true)
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your mail"
+	user_collection.findOne({ mail: req.body.mail })
+		.then(data => {
+			if (data) {
+				res.status(200).send(false)
+			}
+			else {
+				res.status(200).send(true)
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your mail"
+			});
 		});
-	});
 };
 
 
@@ -131,17 +125,17 @@ exports.get_likes_of_user = (req, res) => {
 	}
 
 	// Save User in the database
-	const cursor = like_collection.find({liked_id: req.body.liked_id})
+	const cursor = like_collection.find({ liked_id: req.body.liked_id })
 	const likers = cursor.toArray()
-	.then(data => {
-		res.status(200).send(data)
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your likes"
+		.then(data => {
+			res.status(200).send(data)
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your likes"
+			});
 		});
-	});
 };
 
 exports.get_consults_of_user = (req, res) => {
@@ -153,17 +147,17 @@ exports.get_consults_of_user = (req, res) => {
 	}
 
 	// Save User in the database
-	const cursor = like_collection.find({consulted_id: req.body.consulted_id})
+	const cursor = like_collection.find({ consulted_id: req.body.consulted_id })
 	const likers = cursor.toArray()
-	.then(data => {
-		res.status(200).send(data)
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your consulters"
+		.then(data => {
+			res.status(200).send(data)
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your consulters"
+			});
 		});
-	});
 };
 
 exports.get_blocks_of_and_by_user = (req, res) => {
@@ -177,20 +171,20 @@ exports.get_blocks_of_and_by_user = (req, res) => {
 	// Save User in the database
 	const cursor = like_collection.find({
 		$or: [
-			{blocked_id: req.body.user_id},
-			{blocker_id: req.body.user_id},
+			{ blocked_id: req.body.user_id },
+			{ blocker_id: req.body.user_id },
 		]
 	})
 	const likers = cursor.toArray()
-	.then(data => {
-		res.status(200).send(data)
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your blocks"
+		.then(data => {
+			res.status(200).send(data)
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your blocks"
+			});
 		});
-	});
 };
 
 
@@ -205,21 +199,20 @@ exports.like_user = (req, res) => {
 	}
 
 	// Save User in the database
-	like_collection.findOne({liked_id: req.body.liked_id, liker_id: req.body.liker_id})
-	.then(data => {
-		if (data == null)
-		{
-			like_collection.insertOne({liked_id: req.body.liked_id, liker_id: req.body.liker_id})
-			.then(data => {res.send(data)})
-			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your like"}))
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your like redundancy"
+	like_collection.findOne({ liked_id: req.body.liked_id, liker_id: req.body.liker_id })
+		.then(data => {
+			if (data == null) {
+				like_collection.insertOne({ liked_id: req.body.liked_id, liker_id: req.body.liker_id })
+					.then(data => { res.send(data) })
+					.catch(err => res.status(500).send({ message: err.message || "Some error occurred while sending your like" }))
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your like redundancy"
+			});
 		});
-	});
 };
 
 exports.block_user = (req, res) => {
@@ -231,21 +224,20 @@ exports.block_user = (req, res) => {
 	}
 
 	// Save User in the database
-	block_collection.findOne({blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id})
-	.then(data => {
-		if (data == null)
-		{
-			block_collection.insertOne({blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id})
-			.then(data => {res.send(data)})
-			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your block"}))
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your block redundancy"
+	block_collection.findOne({ blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id })
+		.then(data => {
+			if (data == null) {
+				block_collection.insertOne({ blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id })
+					.then(data => { res.send(data) })
+					.catch(err => res.status(500).send({ message: err.message || "Some error occurred while sending your block" }))
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your block redundancy"
+			});
 		});
-	});
 };
 
 exports.consult_user = (req, res) => {
@@ -257,19 +249,45 @@ exports.consult_user = (req, res) => {
 	}
 
 	// Save User in the database
-	consult_collection.findOne({consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id})
-	.then(data => {
-		if (data == null)
-		{
-			consult_collection.insertOne({consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id})
-			.then(data => {res.send(data)})
-			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your consult"}))
-		}
-	})
-	.catch(err => {
-		res.status(500).send({
-			message:
-			err.message || "Some error occurred while checking your consult redundancy"
+	consult_collection.findOne({ consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id })
+		.then(data => {
+			if (data == null) {
+				consult_collection.insertOne({ consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id })
+					.then(data => { res.send(data) })
+					.catch(err => res.status(500).send({ message: err.message || "Some error occurred while sending your consult" }))
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while checking your consult redundancy"
+			});
 		});
-	});
+};
+
+exports.login_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.username || !req.body.password) {
+		res.status(400).send({ message: "missing information input "});
+		return;
+	}
+
+	// Save User in the database
+	user_collection.findOne({username: req.body.username})
+		.then(data => {
+			if (data == null || data.password != req.body.password) {
+				res.status(400).send("Username or password wrong")
+			}
+			else
+			{
+				res.send(utils.sanitize_soft(data))
+			}
+		})
+		.catch(err => {
+			res.status(500).send({
+				message:
+					err.message || "Some error occurred while logging you in"
+			});
+		});
 };
--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -2,15 +2,17 @@ module.exports = app => {
 	const userController = require("../controllers/user.controller.js");
 	var router = require("express").Router();
 	// Create a new Tutorial
-	router.post("/create", userController.create);
-	router.get("/checkusername", userController.check_username_free);
-	router.get("/checkmail", userController.check_mail_free);
-	router.get("/like", userController.like_user);
-	router.get("/getlikes", userController.get_likes_of_user);
-	router.get("/block", userController.block_user);
-	router.get("/getblocks", userController.get_blocks_of_and_by_user);
-	router.get("/consult", userController.consult_user);
-	router.get("/getconsults", userController.get_consutls_of_user);
+	router.post("/create", 			userController.create);
+	router.get("/checkusername", 	userController.check_username_free);
+	router.get("/checkmail", 		userController.check_mail_free);
+	router.get("/like", 			userController.like_user);
+	router.get("/getlikes", 		userController.get_likes_of_user);
+	router.get("/block", 			userController.block_user);
+	router.get("/getblocks", 		userController.get_blocks_of_and_by_user);
+	router.get("/consult", 			userController.consult_user);
+	router.get("/login", 			userController.login_user);
+
+	// router.get("/getconsults", 		userController.get_consutls_of_user);
 	// Retrieve all Tutorials
 	// router.get("/", tutorials.findAll);
 	// // Retrieve all published Tutorials
