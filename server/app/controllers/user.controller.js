6a4703ff3a4e76dfced1729567b177a9b6706219
Joep Barmentlo <JoepB@example.com>
2022-03-17 19:06:07 +0100

    basic API in place

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -2,7 +2,13 @@ const { calculateObjectSize } = require("bson");
 const db = require("../newmodels");
 // console.log(db)
 // console.log("SDF")
-const user_collection = db.collection("users");
+const user_collection 	= db.collection("users");
+const like_collection 	= db.collection("likes");
+const block_collection 	= db.collection("blocks");
+const consult_collection= db.collection("consult");
+const tag_collection 	= db.collection("tags");
+
+
 // Create and Save a new User
 exports.create = (req, res) => {
 	// Validate request
@@ -34,6 +40,8 @@ exports.create = (req, res) => {
 		});
 	  });
 };
+
+
 
 checkUsernameSanity = (name) => {
 	try
@@ -83,5 +91,185 @@ exports.check_username_free = (req, res) => {
 	});
 };
 
+exports.check_mail_free = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.mail || req.body.mail.length < 5) {
+		res.status(400).send({ message: "Mail can not be empty or shorter than 5 characters!" });
+		return;
+	}
+
+	// Save User in the database
+	user_collection.findOne({mail: req.body.mail})
+	.then(data => {
+		if (data)
+		{
+			res.status(200).send(false)
+		}
+		else
+		{
+			res.status(200).send(true)
+		}
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your mail"
+		});
+	});
+};
 
 
+
+
+exports.get_likes_of_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.liked_id) {
+		res.status(400).send({ message: "Id missing to look for likes" });
+		return;
+	}
+
+	// Save User in the database
+	const cursor = like_collection.find({liked_id: req.body.liked_id})
+	const likers = cursor.toArray()
+	.then(data => {
+		res.status(200).send(data)
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your likes"
+		});
+	});
+};
+
+exports.get_consults_of_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.consulted_id) {
+		res.status(400).send({ message: "Id missing to look for consults" });
+		return;
+	}
+
+	// Save User in the database
+	const cursor = like_collection.find({consulted_id: req.body.consulted_id})
+	const likers = cursor.toArray()
+	.then(data => {
+		res.status(200).send(data)
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your consulters"
+		});
+	});
+};
+
+exports.get_blocks_of_and_by_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.user_id) {
+		res.status(400).send({ message: "Id missing to look for blocks" });
+		return;
+	}
+
+	// Save User in the database
+	const cursor = like_collection.find({
+		$or: [
+			{blocked_id: req.body.user_id},
+			{blocker_id: req.body.user_id},
+		]
+	})
+	const likers = cursor.toArray()
+	.then(data => {
+		res.status(200).send(data)
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your blocks"
+		});
+	});
+};
+
+
+
+
+exports.like_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.liker_id || !req.body.liked_id) {
+		res.status(400).send({ message: "Id missing to like" });
+		return;
+	}
+
+	// Save User in the database
+	like_collection.findOne({liked_id: req.body.liked_id, liker_id: req.body.liker_id})
+	.then(data => {
+		if (data == null)
+		{
+			like_collection.insertOne({liked_id: req.body.liked_id, liker_id: req.body.liker_id})
+			.then(data => {res.send(data)})
+			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your like"}))
+		}
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your like redundancy"
+		});
+	});
+};
+
+exports.block_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.blocker_id || !req.body.blocked_id) {
+		res.status(400).send({ message: "Id missing to block" });
+		return;
+	}
+
+	// Save User in the database
+	block_collection.findOne({blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id})
+	.then(data => {
+		if (data == null)
+		{
+			block_collection.insertOne({blocked_id: req.body.blocked_id, blocker_id: req.body.blocker_id})
+			.then(data => {res.send(data)})
+			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your block"}))
+		}
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your block redundancy"
+		});
+	});
+};
+
+exports.consult_user = (req, res) => {
+	// Validate request
+	// console.log("req: %s res:%s",req.body, res)
+	if (!req.body.consulter_id || !req.body.consulted_id) {
+		res.status(400).send({ message: "Id missing to consult" });
+		return;
+	}
+
+	// Save User in the database
+	consult_collection.findOne({consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id})
+	.then(data => {
+		if (data == null)
+		{
+			consult_collection.insertOne({consulted_id: req.body.consulted_id, consulter_id: req.body.consulter_id})
+			.then(data => {res.send(data)})
+			.catch(err => res.status(500).send({message: err.message || "Some error occurred while sending your consult"}))
+		}
+	})
+	.catch(err => {
+		res.status(500).send({
+			message:
+			err.message || "Some error occurred while checking your consult redundancy"
+		});
+	});
+};
--- server/app/routes/user.routes.js
+++ server/app/routes/user.routes.js
@@ -4,6 +4,13 @@ module.exports = app => {
 	// Create a new Tutorial
 	router.post("/create", userController.create);
 	router.get("/checkusername", userController.check_username_free);
+	router.get("/checkmail", userController.check_mail_free);
+	router.get("/like", userController.like_user);
+	router.get("/getlikes", userController.get_likes_of_user);
+	router.get("/block", userController.block_user);
+	router.get("/getblocks", userController.get_blocks_of_and_by_user);
+	router.get("/consult", userController.consult_user);
+	router.get("/getconsults", userController.get_consutls_of_user);
 	// Retrieve all Tutorials
 	// router.get("/", tutorials.findAll);
 	// // Retrieve all published Tutorials
