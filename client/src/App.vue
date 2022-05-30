9cdf0e3d732e0878930520aac848b20272928e0c
Joep Barmentlo <JoepB@example.com>
2022-05-30 19:44:16 +0200

    removed cometo for nom

--- client/src/App.vue
+++ client/src/App.vue
@@ -16,7 +16,7 @@ import NavBar from "./components/NavBar.vue"
 import { likesOfMe, likesByMe } from "./services/like.script"
 import { blocksOfMe, blocksByMe } from "./services/block.script"
 import { getMyUserDetails } from "./services/user.script"
-import { CometChat } from "@cometchat-pro/chat";
+// import { CometChat } from "@cometchat-pro/chat";
 // require('dotenv').config();
 
 // process.env.USER_ID; // "239482"
@@ -79,24 +79,24 @@ export default {
       this.updateBlocks()
       this.getCurrentUser()
     }
-    const appID = process.env.VUE_APP_ID;
-    const region = process.env.VUE_APP_REGION;
-    console.log("APPPSDFSDFSDFF: ", appID, region)
-    const appSetting = new CometChat.AppSettingsBuilder()
-      .subscribePresenceForAllUsers()
-      .setRegion(region)
-      .build();
+    // const appID = process.env.VUE_APP_ID;
+    // const region = process.env.VUE_APP_REGION;
+    // console.log("APPPSDFSDFSDFF: ", appID, region)
+    // const appSetting = new CometChat.AppSettingsBuilder()
+    //   .subscribePresenceForAllUsers()
+    //   .setRegion(region)
+    //   .build();
 
-    CometChat.init(appID, appSetting).then(
-      () => {
-        console.log('Initialization completed successfully');
-        // You can now call login function.
-      },
-      (error) => {
-        console.log('Initialization failed with error:', error);
-        // Check the reason for error and take appropriate action.
-      }
-    );
+    // CometChat.init(appID, appSetting).then(
+    //   () => {
+    //     console.log('Initialization completed successfully');
+    //     // You can now call login function.
+    //   },
+    //   (error) => {
+    //     console.log('Initialization failed with error:', error);
+    //     // Check the reason for error and take appropriate action.
+    //   }
+    // );
 	},
 }
 </script>
