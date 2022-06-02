15adfaff380bbae8c8418520ff8a5c97976624d4
Joep Barmentlo <JoepB@example.com>
2022-06-02 14:36:47 +0200

    online cluster

--- server/app/newmodels/index.js
+++ server/app/newmodels/index.js
@@ -1,6 +1,12 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config()
+
+const username = encodeURIComponent(process.env.MONGO_USER);
+const password = encodeURIComponent(process.env.MONGO_KEY);
+
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb://localhost:27017"
+// const uri = "mongodb://localhost:27017"
+const uri = `mongodb+srv://${username}:${password}@cluster0.ft4j5.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri);
 
 async function connect() {
