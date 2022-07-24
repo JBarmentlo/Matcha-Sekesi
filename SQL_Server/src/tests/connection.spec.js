d15f2964a37b11edebe60b25b50b6b3c47d8f1bb
Joep Barmentlo <JoepB@example.com>
2022-07-24 13:12:04 +0200

    mocha now exits

--- SQL_Server/src/tests/connection.spec.js
+++ SQL_Server/src/tests/connection.spec.js
@@ -6,8 +6,10 @@ const sinon		= require('sinon');
 const db		= require('../db/sql.conn')
 
 describe("Test db connection exists.", () => {
-	describe('db.connection._eventsCount test', async () => {
-		let conn = await db.conn
-		assert.equal(db.connection._eventsCount, 1)
+	describe('db.connection._eventsCount test', () => {
+		it('Should be equal to 1', async () => {
+			let conn = await db.conn
+			assert.equal(conn.connection._eventsCount, 1)
+		})
 	})
 })
