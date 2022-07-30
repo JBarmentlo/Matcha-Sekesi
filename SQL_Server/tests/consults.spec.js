a1d67a8f8443b9b86f55c109834108fb38d29a5b
Joep B <joepbarmentlo@gmail.com>
2022-07-30 15:14:47 +0200

    added Consults

--- SQL_Server/tests/consults.spec.js
+++ SQL_Server/tests/consults.spec.js
@@ -1,127 +1,107 @@
-// const { assert }                  = require('chai');
+const { assert }                  = require('chai');
 
-// const ConsultController           = require('../src/controllers/consult.controller')
-// const UserController              = require('../src/controllers/user.controller')
-// const users                       = require('./data/users.mock')
-// const {mockResponse, mockRequest} = require('./data/res.req.mock')
+const ConsultController              = require('../src/controllers/consult.controller')
+const UserController              = require('../src/controllers/user.controller')
+const users                       = require('./data/users.mock')
+const {mockResponse, mockRequest} = require('./data/res.req.mock')
 
-// describe('Test likes', () => {
-// 	let res = mockResponse()
-// 	let jhonny_id
-// 	let bella_id
-// 	let mark_id
-	
-// 	describe('Get user id by username', () => {
-// 			it('Jhonny status 200', async () => {
-// 				await UserController.get_user_by_username(mockRequest({username: users.Jhonny.username}), res)
-// 				jhonny_id = res.send.lastCall.firstArg.data.id
-// 				assert(res.status.lastCall.args == 200)
-// 			})
-// 			it('Bella status 200', async () => {
-// 				await UserController.get_user_by_username(mockRequest({username: users.Bella.username}), res)
-// 				bella_id = res.send.lastCall.firstArg.data.id
-// 				assert(res.status.lastCall.args == 200)
-// 			})
-// 			it('Mark status 200', async () => {
-// 			await UserController.get_user_by_username(mockRequest({username: users.Mark.username}), res)
-// 			mark_id = res.send.lastCall.firstArg.data.id
-// 			assert(res.status.lastCall.args == 200)
-// 		})
-// 	})
-// 	describe("Create Lonely Mark", () => {
-// 			it('mark   => jhonny Code SUCCESS', async () => {
-// 				await LikeController.like_user(mockRequest({liker_id: mark_id, liked_id: jhonny_id}), res)
-// 				assert(res.send.lastCall.firstArg.code == "SUCCESS")
-// 			})
-// 			it('mark   => bella Code Success', async ()  => {
-// 				await LikeController.like_user(mockRequest({liker_id: mark_id, liked_id: bella_id}), res)
-// 				assert(res.send.lastCall.firstArg.code == "SUCCESS")
-// 			})
-// 			it('bella  => jhonny Code Success', async () => {
-// 				await LikeController.like_user(mockRequest({liker_id: bella_id, liked_id: jhonny_id}), res)
-// 				assert(res.send.lastCall.firstArg.code == "SUCCESS")
-// 			})
-// 			it('jhonny => bella Code Success', async ()  => {
-// 				await LikeController.like_user(mockRequest({liker_id: jhonny_id, liked_id: bella_id}), res)
-// 				assert(res.send.lastCall.firstArg.code == "SUCCESS")
-// 			})
-// 		})
-// 	describe("Like Error Handling", () => {
-// 		describe("duplicate like", () => {
-// 			it('Code ER_DUP_ENTRY', async () => {
-// 				await LikeController.like_user(mockRequest({liker_id: mark_id, liked_id: jhonny_id}), res)
-// 				assert.equal(res.send.lastCall.firstArg.code, "ER_DUP_ENTRY")
-// 			})
-// 		})
-// 		describe("Missing liked", () => {
-// 			it('Code LIKE_MISS', async () => {
-// 				await LikeController.like_user(mockRequest({liker_id: mark_id, liked_id: 'lol'}), res)
-// 				assert.equal(res.send.lastCall.firstArg.code,  "LIKE_MISS")
-// 			})
-// 		})
-// 		describe("Missing liker", () => {
-// 			it('Code LIKE_MISS', async () => {
-// 				await LikeController.like_user(mockRequest({liker_id: 'lol', liked_id: jhonny_id}), res)
-// 				assert.equal(res.send.lastCall.firstArg.code,  "LIKE_MISS")
-// 			})
-// 		})
-// 	})
-// 	describe("Get Liked Users", () => {
-// 		describe("Get Marks liked users", () => {
-// 			it("Includes bella and jhonny", async () => {
-// 				let reso = mockResponse()
-// 				await LikeController.get_users_that_i_liked(mockRequest({liker_username: users.Mark.username}), reso)
-// 				yusers = reso.send.lastCall.firstArg.data.map(function(a) {return a.username})
-// 				assert.isTrue(yusers.includes(users.Bella.username))
-// 				assert.isTrue(yusers.includes(users.Jhonny.username))
-// 			})
-// 		})
-// 	})
-// 	describe("Get Users that liked me", () => {
-// 		describe("Get users that like bella", () => {
-// 			it("Includes Mard and jhonny", async () => {
-// 				let reso = mockResponse()
-// 				await LikeController.get_users_that_liked_me(mockRequest({liked_username: users.Bella.username}), reso)
-// 				yusers = reso.send.lastCall.firstArg.data.map(function(a) {return a.username})
-// 				assert.isTrue(yusers.includes(users.Mark.username))
-// 				assert.isTrue(yusers.includes(users.Jhonny.username))
-// 			})
-// 		})
-// 	})
-// 	describe("Get Matches", () => {
-// 		describe("Bella's matches", () => {
-// 			it("jhonny", async () => {
-// 				let reso = mockResponse()
-// 				await LikeController.get_matches(mockRequest({username: users.Bella.username}), reso)
-// 				assert.isTrue(reso.send.lastCall.firstArg.data.includes(users.Jhonny.username))
-// 			})
-// 		})
-// 		describe("Marks's matches", () => {
-// 			it("All Alone HAHA", async () => {
-// 				await LikeController.get_matches(mockRequest({username: users.Mark.username}), res)
-// 				// assert.isTrue(reso.send.lastCall.firstArg.data.includes(users.Jhonny.username))
-// 				assert.equal(res.send.lastCall.firstArg.data.length, 0)
-// 			})
-// 		})
-// 	})
-// 	describe("Jhonny unlike bella", () => {
-// 		it("affects 1 row", async () => {
-// 			await LikeController.un_like_user(mockRequest({unliker: users.Jhonny.username, unliked: users.Bella.username}), res)
-// 			assert.equal(res.send.lastCall.firstArg.data.affectedRows, 1)
-// 		})
-// 	})
-// 	describe("Bella's likers", () => {
-// 		it("Jhonny's gone !", async () => {
-// 			await LikeController.get_users_that_liked_me(mockRequest({liked_username: users.Bella.username}), res)
-// 			assert.isFalse(res.send.lastCall.firstArg.data.includes(users.Jhonny.username))
-// 		})
-// 	})
-// 	describe("Bella's matches", () => {
-// 		it("Bye Bye Jhonny", async () => {
-// 			await LikeController.get_matches(mockRequest({username: users.Bella.username}), res)
-// 			assert.isFalse(res.send.lastCall.firstArg.data.includes(users.Jhonny.username))
-// 		})
-// 	})
-// })
+describe('Test consults', () => {
+	let res = mockResponse()
 
-// // MISSING_LIKE
+	describe("Create Consults", () => {
+		step('mark   => jhonny Code SUCCESS', async () => {
+			await ConsultController.consult_user(mockRequest({consulter: users.Mark.username, consulted: users.Jhonny.username}), res)
+			assert.equal(res.send.lastCall.firstArg.code, "SUCCESS")
+		})
+		step('mark   => bella Code Success', async ()  => {
+			await ConsultController.consult_user(mockRequest({consulter: users.Mark.username, consulted: users.Bella.username}), res)
+			assert.equal(res.send.lastCall.firstArg.code, "SUCCESS")
+		})
+		step('bella  => jhonny Code Success', async () => {
+			await ConsultController.consult_user(mockRequest({consulter: users.Bella.username, consulted: users.Jhonny.username}), res)
+			assert.equal(res.send.lastCall.firstArg.code, "SUCCESS")
+		})
+		step('jhonny => bella Code Success', async ()  => {
+			await ConsultController.consult_user(mockRequest({consulter: users.Jhonny.username, consulted: users.Bella.username}), res)
+			assert.equal(res.send.lastCall.firstArg.code, "SUCCESS")
+		})
+	})
+	describe("Consult Error Handling", () => {
+		describe("duplicate consult", () => {
+			step('Code ER_DUP_ENTRY', async () => {
+				await ConsultController.consult_user(mockRequest({consulter: users.Mark.username, consulted: users.Jhonny.username}), res)
+				assert.equal(res.send.lastCall.firstArg.code, "ER_DUP_ENTRY")
+			})
+		})
+		describe("Missing consulted", () => {
+			step('Code LIKE_MISS', async () => {
+				await ConsultController.consult_user(mockRequest({consulter: users.Mark.username, consulted: 'lol'}), res)
+				assert.equal(res.send.lastCall.firstArg.code,  "ER_NO_REFERENCED_ROW")
+			})
+		})
+		describe("Missing consulter", () => {
+			step('Code LIKE_MISS', async () => {
+				await ConsultController.consult_user(mockRequest({consulter: 'lol', consulted: users.Jhonny.username}), res)
+				assert.equal(res.send.lastCall.firstArg.code,  "ER_NO_REFERENCED_ROW")
+			})
+		})
+	})
+	describe("Get consulted Users", () => {
+		describe("Get Marks consulted users", () => {
+			step("Includes bella and jhonny", async () => {
+				let reso = mockResponse()
+				await ConsultController.get_users_that_i_consulted(mockRequest({consulter_username: users.Mark.username}), reso)
+				yusers = reso.send.lastCall.firstArg.data.map(function(a) {return a.username})
+				assert.isTrue(yusers.includes(users.Bella.username))
+				assert.isTrue(yusers.includes(users.Jhonny.username))
+			})
+		})
+	})
+	describe("Get Users that consulted me", () => {
+		describe("Get users that consult bella", () => {
+			step("Includes Mard and jhonny", async () => {
+				let reso = mockResponse()
+				await ConsultController.get_users_that_consulted_me(mockRequest({consulted_username: users.Bella.username}), reso)
+				yusers = reso.send.lastCall.firstArg.data.map(function(a) {return a.username})
+				assert.isTrue(yusers.includes(users.Mark.username))
+				assert.isTrue(yusers.includes(users.Jhonny.username))
+			})
+		})
+	})
+	describe("Get Matches", () => {
+		describe("Bella's matches", () => {
+			step("jhonny", async () => {
+				let reso = mockResponse()
+				await ConsultController.get_consult_matches(mockRequest({username: users.Bella.username}), reso)
+				assert.isTrue(reso.send.lastCall.firstArg.data.includes(users.Jhonny.username))
+			})
+		})
+		describe("Marks's matches", () => {
+			step("All Alone HAHA", async () => {
+				await ConsultController.get_consult_matches(mockRequest({username: users.Mark.username}), res)
+				// assert.isTrue(reso.send.lastCall.firstArg.data.includes(users.Jhonny.username))
+				assert.equal(res.send.lastCall.firstArg.data.length, 0)
+			})
+		})
+	})
+	describe("Jhonny unconsult bella", () => {
+		step("affects 1 row", async () => {
+			await ConsultController.un_consult_user(mockRequest({unconsulter: users.Jhonny.username, unconsulted: users.Bella.username}), res)
+			assert.equal(res.send.lastCall.firstArg.data.affectedRows, 1)
+		})
+	})
+	describe("Bella's consulters", () => {
+		step("Jhonny's gone !", async () => {
+			await ConsultController.get_users_that_consulted_me(mockRequest({consulted_username: users.Bella.username}), res)
+			assert.isFalse(res.send.lastCall.firstArg.data.includes(users.Jhonny.username))
+		})
+	})
+	describe("Bella's matches", () => {
+		step("Bye Bye Jhonny", async () => {
+			await ConsultController.get_consult_matches(mockRequest({username: users.Bella.username}), res)
+			assert.isFalse(res.send.lastCall.firstArg.data.includes(users.Jhonny.username))
+		})
+	})
+})
+
+// MISSING_LIKE
--- SQL_Server/tests/hooks.js
+++ SQL_Server/tests/hooks.js
@@ -1,14 +1,25 @@
+const {mockResponse, mockRequest} = require('./data/res.req.mock')
+const UserController              = require('../src/controllers/user.controller')
+const users                       = require('./data/users.mock')
+
 exports.mochaHooks = {
-	beforeAll () {
+	async beforeAll () {
 		process.env.MATCHA_DB = "sekesitest"
 		process.env.TEST = "true"
 		test_con = require('../src/controllers/test.controller')
-		return test_con.clear_db()
+		await test_con.clear_db()
+		let res = mockResponse()
+		let req = mockRequest(users.Jhonny)
+		await UserController.create_user_test(req, res)
+		req = mockRequest(users.Bella)
+		await UserController.create_user_test(req, res)
+		req = mockRequest(users.Mark)
+		await UserController.create_user_test(req, res)
 	},
 	
 	async afterAll() {
 		test_con = require('../src/controllers/test.controller')
-		await test_con.clear_db()
+		// await test_con.clear_db()
 		process.env.MATCHA_DB = "sekesidbd"
 		process.env.TEST = "false"
 	}
