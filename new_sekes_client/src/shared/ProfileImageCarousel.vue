030168ac12130d7ac904f7f35af8cca94590be7a
Joep B <joepbarmentlo@gmail.com>
2022-09-24 14:04:27 +0200

    starting move of image fully to carousel

--- new_sekes_client/src/shared/ProfileImageCarousel.vue
+++ new_sekes_client/src/shared/ProfileImageCarousel.vue
@@ -11,7 +11,7 @@
       img-height="480"
       style="text-shadow: 1px 1px 2px #333;"
     >
-    <div v-for="url in images" :key="url">
+    <div v-for="url in user_images" :key="url">
       <b-carousel-slide
             :img-src=url>
       </b-carousel-slide>
@@ -43,53 +43,67 @@
 import { uploadImage }from '../services/upload'
 
 
-  export default {
-    props: ['images'],
-    data() {
-      return {
-        slide: 0,
-        image_urls: this.images,
-        selected_file : null,
-        message: null
-      }
-    },
-    methods: {
-        emitDeleteCurrentSlide() {
-            this.$emit("DeleteImage", this.slide)
-        },
-
-        EmitAddImage() {
-            this.$emit("AddImage")
-        },
-
-        async UploadAndAddImage(e) {
-            const file = e.target.files[0]
-            this.selected_file = file
-            console.log("file selected: ", this.selected_file)
-            if (this.selected_file == null) {
-              console.log("WIERD SELECT NO FILE BUT CHANGE")
-              return
-            }
-            else {
-              try {
-                let upload_res = await uploadImage(this.$cookies.get('sekes_tokens'), this.selected_file)
-                console.log("RESII: ",upload_res)
-                this.message = "Upload complete"
-                this.$emit("AddImage", upload_res.data.url)
-              }
-              catch {
-                this.message = "Error"
-                console.log("ERR")
-              }
-              finally {
-                this.selected_file = null
-              }
-            }
-        }
-    },
-    mounted() {
-        console.log("IMAGEGE: ", this.image_urls)
-
+export default {
+  props: ['images'],
+  data() {
+    return {
+      slide: 0,
+      image_urls: this.images,
     }
+  },
+
+  computed: {
+      user_images: function() {
+        return this.images.filter((im) => {return (im != undefined & im != null)})
+      },
+
+      user_image_indexes: function() {
+        let indexes = []
+        for (let i = 0; i < 4; i++) {
+            if (this.images[i] != null) {
+                indexes.push(i)
+            }
+        } 
+        return indexes
+      }
+  },
+
+  methods: {
+      emitDeleteCurrentSlide() {
+          this.$emit("DeleteImage", this.user_image_indexes[this.slide])
+      },
+
+      EmitAddImage(image_url) {
+          console.log("adding image: ", image_url)
+          for (let i = 0; i < 4; i++) {
+              if (this.images[i] == null) {
+                this.$emit("AddImage", image_url, i)
+                return
+              }
+          }
+          console.log("EROOOOR IN ADDIMAGES")
+      },
+
+      async UploadAndAddImage(e) {
+          const file = e.target.files[0]
+          if (file == null) {
+            console.log("WIERD SELECT NO FILE BUT CHANGE")
+            return
+          }
+          else {
+            try {
+              let upload_res = await uploadImage(this.$cookies.get('sekes_tokens'), file)
+              this.$emit("AddImage", upload_res.data.url)
+            }
+            catch {
+              console.log("ERR")
+            }
+          }
+      },
+
+      mounted() {
+        console.log("IM", this.user_images)
+      }
   }
+}
 </script>
