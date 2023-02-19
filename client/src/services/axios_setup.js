521e971fb785bcc9fcbca66dc6f4a140f1224359
Joep B <joepbarmentlo@gmail.com>
2023-02-19 14:05:44 +0100

    nicer env vars

--- client/src/services/axios_setup.js
+++ client/src/services/axios_setup.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// const api_address = `${process.env.VUE_APP_MATCHA_HOST}${process.env.VUE_APP_MATCHA_DEFAULT_PORT == '80' || process.env.VUE_APP_MATCHA_DEFAULT_PORT == '443' ? '' : ':' + process.env.VUE_APP_MATCHA_DEFAULT_PORT}`
 const api_address = process.env.VUE_APP_MATCHA_SERVER_HOST
 export const api_axios = axios.create({
     baseURL: api_address
